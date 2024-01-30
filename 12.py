27c74a284740a89f49ef884a71a22f6a373db591
JakubKupiszewski <kkupiszewki@gmail.com>
2024-01-30 07:52:36 +0100

    Poprawka kodu

--- 12.py
+++ 12.py
@@ -1,12 +1,13 @@
 name_set = set()
+
 name_set.add("Kamil")
 name_set.add("Kuba")
 
 name_set2 = {"Mariusz", "Wojtek"}
 
 name_set.update(name_set2)
-#można również extend
+#Możesz dodać też extend
 
-for name in name_set:
-    print(name)
+for name in (name_set):
+   print(name)
 
